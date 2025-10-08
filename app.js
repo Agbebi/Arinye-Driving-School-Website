0d77da582c8bd125e0641bf55c512501543bc1ee
Agbebi Timothy <agbebitimothy8@gmail.com>
2025-10-08 04:36:05 +0100

    Effected public folder

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function (req, res) {  
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile("index.html");
 });
 
 app.post("/", function (req, res) {
